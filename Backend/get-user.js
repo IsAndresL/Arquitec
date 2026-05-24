9c7f08ff34fdb814b83c867107054f59959daf19
isAndresL <andresfluna03@gmail.com>
2026-05-24 15:58:31 -0500

    chore: remove temporary test files and CLAUDE.md boilerplate

--- Backend/get-user.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function getUserId() {
-  const user = await prisma.user.findFirst();
-  console.log(user?.id);
-}
-getUserId();
